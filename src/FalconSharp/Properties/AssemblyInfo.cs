51149f8fecb55d60714ba60328230d622d80a29d
leekelleher <leekelleher@gmail.com>
2014-09-12 12:44:23 +0100

    Fixed up the AssemblyInfo manifests.
    Set the version number to contain the build time/info.

--- src/FalconSharp/Properties/AssemblyInfo.cs
+++ src/FalconSharp/Properties/AssemblyInfo.cs
@@ -1,36 +1,16 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("FalconSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("C#.NET library for the Falcon Social API")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Umbrella Inc Ltd")]
 [assembly: AssemblyProduct("FalconSharp")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright \xa9 Umbrella Inc Ltd 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+[assembly: Guid("FE7331F3-08C6-440B-9E3E-F2076C2A73AE")]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fe7331f3-08c6-440b-9e3e-f2076c2a73ae")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
