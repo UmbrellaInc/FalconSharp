5114308d09dd27a485ca905f6aacb6a801d798ad
leekelleher <leekelleher@gmail.com>
2015-09-01 13:29:04 +0100

    [WIP] Exploring how to mock the API via a unit-test.

--- tests/FalconSharp.Tests/FalconClientTests.cs
+++ tests/FalconSharp.Tests/FalconClientTests.cs
@@ -1,6 +1,7 @@
-﻿using NUnit.Framework;
-using System;
-using System.Linq;
+﻿using System;
+using System.Net.Http;
+using System.Web.Http;
+using NUnit.Framework;
 
 namespace FalconSharp.Tests
 {
@@ -33,8 +34,8 @@ namespace FalconSharp.Tests
             var channelIds = new[] { "instagram_3350_1111721537" };
 
             var feed = client.GetContentFeed(
-                since: new DateTime(2014,01,01),
-                until: new DateTime(2015,05,31),
+                since: new DateTime(2014, 01, 01),
+                until: new DateTime(2015, 05, 31),
                 limit: 100,
                 sort: Models.SortOrder.Desc,
                 channels: channelIds,
@@ -45,6 +46,33 @@ namespace FalconSharp.Tests
             //var content = client.GetChannelContent(channelIds.First());
             //Assert.That(content, Is.Not.Null);
             //Assert.That(content.Items, Is.Not.Null);
+        }
+
+        [Test]
+        public void SelfHostedTest_WIP()
+        {
+            var baseAddress = new Uri("http://localhost:8888");
+            var config = new HttpConfiguration();
+
+            config.Routes.MapHttpRoute(
+                name: "FalconApi_Channels",
+                routeTemplate: "api/falcon/channels",
+                defaults: new { controller = "MockFalconApi", action = "channels" }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "FalconApi_Content",
+                routeTemplate: "api/falcon/channels/{channelId}/content",
+                defaults: new { controller = "MockFalconApi", action = "content" }
+            );
+
+            var server = new HttpServer(config);
+            using (var client = new HttpClient(server))
+            {
+                client.BaseAddress = baseAddress;
+                var response = client.GetAsync("/api/falcon/channels").Result;
+                Assert.True(response.IsSuccessStatusCode, "Actual status code: " + response.StatusCode);
+            }
         }
     }
 }
