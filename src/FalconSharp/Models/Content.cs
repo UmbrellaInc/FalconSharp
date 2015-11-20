c5b977833b8c6384c02621c7f1331053267d97b5
leekelleher <leekelleher@gmail.com>
2015-11-20 18:17:33 +0000

    Started work on implementing the wrapper for the new API.

--- src/FalconSharp/Models/Content.cs
+++ src/FalconSharp/Models/Content.cs
@@ -12,67 +12,37 @@ namespace FalconSharp.Models
         [JsonProperty("network")]
         public string Network { get; set; }
 
-        //[JsonProperty("status")]
-        //public ContentStatus Status { get; set; }
-
-        //[JsonProperty("name")]
-        //public string Name { get; set; }
-
-        //[JsonProperty("description")]
-        //public string Description { get; set; }
-
         [JsonProperty("message")]
         public string Message { get; set; }
-
-        [JsonProperty("picture")]
-        public string Picture { get; set; }
-
-        [JsonProperty("video")]
-        public string Video { get; set; }
-
-        //[JsonProperty("caption")]
-        //public string Caption { get; set; }
-
-        //[JsonProperty("link")]
-        //public string Link { get; set; }
-
-        [JsonProperty("href")]
-        public string Href { get; set; }
-
-        //[JsonProperty("orgId")]
-        //public string OrgId { get; set; }
-
-        //[JsonProperty("fromId")]
-        //public string FromId { get; set; }
-
-        //[JsonProperty("fromName")]
-        //public string FromName { get; set; }
-
-        [JsonProperty("falcon")]
-        public UserSummary Falcon { get; set; }
-
-        [JsonProperty("channel")]
-        public ChannelSummary Channel { get; set; }
 
         [JsonProperty("type")]
         public ContentType Type { get; set; }
 
-        [JsonProperty("tags")]
-        public IEnumerable<string> Tags { get; set; }
+        [JsonProperty("publicationDate")]
+        public DateTime PublicationDate { get; set; }
+
+        [JsonProperty("href")]
+        public string Href { get; set; }
+
+        [JsonProperty("channel")]
+        public ChannelSummary Channel { get; set; }
 
         [JsonProperty("stats")]
-        public IDictionary<string, object> Stats { get; set; }
+        public ContentStats Stats { get; set; }
+
+        [JsonProperty("falcon")]
+        public UserSummary Falcon { get; set; }
+
+        [JsonProperty("tags")]
+        public IEnumerable<string> Tags { get; set; }
 
         [JsonProperty("createdDate")]
         public DateTime CreatedDate { get; set; }
 
-        [JsonProperty("modifiedDate")]
-        public DateTime ModifiedDate { get; set; }
+        [JsonProperty("picture")]
+        public string Picture { get; set; }
 
-        [JsonProperty("publicationDate")]
-        public DateTime PublicationDate { get; set; }
-
-        //[JsonProperty("unPublished")]
-        //public bool Unpublished { get; set; }
+        [JsonProperty("link")]
+        public string Link { get; set; }
     }
 }
---
+++ src/FalconSharp/Models/ContentStats.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace FalconSharp.Models
+{
+    public class ContentStats
+    {
+        [JsonProperty("comments")]
+        public int Comments { get; set; }
+
+        [JsonProperty("likes")]
+        public int Likes { get; set; }
+
+        [JsonProperty("shares")]
+        public int Shares { get; set; }
+    }
+}
